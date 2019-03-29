9165996f80d58a1c84631f37682a744c3dfabceb
Guus <guus.dijkhuis@gmail.com>
2019-03-29 12:55:28 +0100

    add feature detection js

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,36 @@
-const submitbtn = document.querySelector('#submit-btn').style.display = "none";
-const allInputs = document.querySelectorAll('input[type=number]')
+var submitBtn, inputArr
 
-const inputArr = Array.prototype.slice.call(allInputs)
+if (document.querySelector) {
+   submitBtn = document.querySelector('#submit-btn')
+  if (submitBtn.style) {
+    submitBtn.style.display = "none";
+  }
+} else {
+  document.getElementById('submit-btn')
+}
 
-const Http = new XMLHttpRequest()
-const url = '/change-score-client'
+if (document.querySelectorAll) {
+  var allInputs = document.querySelectorAll('input[type=number]')
+  if (Array.from) {
+    inputArr = Array.from(allInputs)
+  } else {
+    inputArr = Array.prototype.slice.call(allInputs)
+  }
+} else {
+  var allInputs = document.getElementsByClassName('inputField')
+  if (Array.from) {
+    inputArr = Array.from(allInputs)
+  } else {
+    inputArr = Array.prototype.slice.call(allInputs)
+  }
+}
+
+var Http = new XMLHttpRequest()
+var url = '/change-score-client'
 
 function pushData() {
-  const obj = {
+  console.log('s');
+  var obj = {
     homeTeam: {
       name: "Ajax",
       logo: "/img/AFCA-logo.png",
@@ -30,6 +53,6 @@ function pushData() {
   Http.send(JSON.stringify(obj))
 }
 
-inputArr.forEach(function(res) {
-  res.addEventListener('change', pushData)
-})
+for(var i = 0; i < inputArr.length; i++) {
+  inputArr[i].addEventListener('change', pushData)
+}
